9c455003084c930ea001a099968b0f443d292682
lakihaomal <lhw150030@utdallas.edu>
2023-06-16 16:51:41 -0500

    referenceLightModule 1st update

---
+++ firmware/xu4LoRa/l_1_readOOSR2.py
@@ -0,0 +1,102 @@
+
+# MQTT Client demo
+# Continuously monitor two different MQTT topics for data,
+# check if the received data matches two predefined 'commands'
+import itertools
+import base64
+from cgitb import strong
+# import imp
+# from this import d
+import paho.mqtt.client as mqtt
+import datetime 
+from datetime import timedelta
+import yaml
+import collections
+import json
+import time 
+import serial.tools.list_ports
+from collections import OrderedDict
+from glob import glob
+from mintsXU4 import mintsDefinitions as mD
+from mintsXU4 import mintsPoLo as mPL
+from collections import OrderedDict
+import struct
+import numpy as np
+import pynmea2
+import shutil
+
+
+from __future__ import annotations
+from oceandirect.OceanDirectAPI import OceanDirectAPI, OceanDirectError
+from oceandirect.od_logger import od_logger
+from threading import Thread
+logger = od_logger()
+od = OceanDirectAPI()
+
+from mintsXU4 import mintsOptics as mO
+
+import math
+import sys
+import time
+import os
+
+debug  = False 
+
+devicesPresent = False
+deviceOpen = False
+
+macAddress          = mD.macAddress
+
+electricDarkCorrelationUsage = False
+nonLinearityCorrectionUsage  = True
+integrationTimeMicroSec      = 1000 
+
+
+if __name__ == "__main__":
+    
+    print()
+    print("============ MINTS Reference Light Module ============")
+    print()
+
+    devicesPresent, deviceIDs = mO.checkingDevicePresence()
+    device, deviceOpen, deviceID, deviceSerialNumber = \
+        mO.openDevice(deviceOpen,devicesPresent,deviceIDs)
+    
+    mO.setUpDevice(devicesPresent, deviceOpen, device,\
+                    electricDarkCorrelationUsage,\
+                    nonLinearityCorrectionUsage,\
+                    integrationTimeMicroSec,\
+                    )
+    
+
+    mO.getSingleSpectrum(devicesPresent, deviceOpen,device)
+
+
+    # # I2C Devices 
+    # scd30Online    = scd30.initiate(30)
+    # as7265xOnline  = as7265x.initiate()
+    
+
+   
+    # while True:
+    #     try:    
+
+
+
+        # except Exception as e:
+        #     time.sleep(.5)
+        #     print ("Error and type: %s - %s." % (e,type(e)))
+        #     time.sleep(.5)
+        #     print("Data Packet Not Sent")
+        #     time.sleep(.5)
+
+                  
+        
+        
+        
+
+        
+        
+        
+        
+        
---
+++ firmware/xu4LoRa/mintsXU4/mintsOptics.py
@@ -0,0 +1,137 @@
+
+import serial
+import datetime
+import os
+import csv
+#import deepdish as dd
+# from mintsXU4 import mintsLatest as mL
+from mintsXU4 import mintsDefinitions as mD
+# from mintsXU4 import mintsSensorReader as mSR
+from getmac import get_mac_address
+import time
+import serial
+import pynmea2
+from collections import OrderedDict
+import math
+import base64
+import json
+import struct
+import numpy as np
+from datetime import timedelta
+
+from __future__ import annotations
+from oceandirect.OceanDirectAPI import OceanDirectAPI, OceanDirectError
+from oceandirect.od_logger import od_logger
+from threading import Thread
+
+
+macAddress     = mD.macAddress
+dataFolder     = mD.dataFolder
+fPortIDs        = mD.fPortIDs
+
+mqttOn         = mD.mqttOn
+decoder        = json.JSONDecoder(object_pairs_hook=OrderedDict)
+od = OceanDirectAPI()
+
+# Gives the first device it detects 
+def checkingDevicePresence():
+    od.find_usb_devices()
+
+    deviceIDs   = od.get_device_ids()
+    deviceCount = len(deviceIDs)
+    if deviceCount:
+        print("Ocean Optics devices found")
+        return True,deviceIDs ;
+    else:
+        print("Ocean Optics devices not found")
+        return False, [];
+
+def openDevice(deviceOpen,devicesPresent,deviceIDs):
+
+    if devicesPresent:
+        print("Ocean Optics Devices found:")
+        if not(deviceOpen):
+            print("Opening Device")
+            device       = od.open_device(id)
+        else:
+            print("Device Already Opened")
+
+        # Provide the first ID it finds
+        for deviceID in deviceIDs:
+            deviceSerialNumber = device.get_serial_number()
+            print("Device Serial Number: %s" % deviceSerialNumber)
+            return device,True,deviceID,deviceSerialNumber
+
+    print("No devices found")
+    return [],deviceOpen,[],[];
+
+def closeDevice(devicesOpen,devicesPresent,deviceID):
+    if devicesOpen and devicesPresent:
+        od.close_device(deviceID);
+
+
+
+def setUpDevice(devicesPresent, deviceOpen,device,\
+                electricDarkCorrelationUsage,\
+                nonLinearityCorrectionUsage,\
+                integrationTimeMicroSec,\
+                ):
+    if devicesPresent:
+        print("Devices Present")
+        if deviceOpen:
+            print("Device Open")
+            print("Setting Up Device")
+            device.set_electric_dark_correction_usage(electricDarkCorrelationUsage)
+            device.set_nonlinearity_correction_usage(nonLinearityCorrectionUsage)
+            device.set_integration_time(integrationTimeMicroSec)
+        else:
+            print("Device not open")    
+    else:
+        print("No Devices found to setup")    
+
+def getSingleSpectrum(devicesPresent, deviceOpen,device):
+    if devicesPresent:
+        print("Devices Present")
+        if deviceOpen:
+            print("Device Open")
+            print("Setting Up Device")
+            try: 
+                spectra_m = device.get_formatted_spectrum()
+                print(spectra_m)
+                return spectra_m;
+            except OceanDirectError as e:
+                print("Error Reading Data")
+                print(e.get_error_details())
+                return [];
+        else:
+            print("Device not open")    
+    else:
+        print("No Devices found to setup")   
+
+
+        
+
+def getSpectraSingle(devicesPresent, deviceOpen,\
+                     device,numbSpectra,logger):
+    if devicesPresent:
+        print("Devices Present")
+        if deviceOpen:
+            print("Device Open")
+            print("Setting Up Device")    
+            try: 
+                device.set_scans_to_average(1)
+                numb_pixel = len(device.get_formatted_spectrum()) 
+                spectra_m = [[0 for x in range(numb_pixel)] for y in range(numbSpectra)]
+                for i in range(numbSpectra):
+                    spectra_m[i] = device.get_formatted_spectrum()
+                return spectra_m;
+
+            except OceanDirectError as e:
+                logger.error(e.get_error_details())
+                return;
+
+        else:
+            print("Device not open")    
+    else:
+        print("No Devices found to setup")   
+
