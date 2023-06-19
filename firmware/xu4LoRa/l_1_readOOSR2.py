1c59adae00f39c03b91bb8bd84a83a76b7f305ae
lakihaomal <lhw150030@utdallas.edu>
2023-06-19 10:12:20 -0500

    referenceLightModule 1st update

--- firmware/xu4LoRa/l_1_readOOSR2.py
+++ firmware/xu4LoRa/l_1_readOOSR2.py
@@ -88,7 +88,7 @@ if __name__ == "__main__":
                   + "Spectrum read at: " + str(dateTime) 
         
         plt.title(titleStr)
-        plt.savefig("/home/teamlary/Pictures/"+titleStr.replace(" ","")+".png")
+        plt.savefig("/home/teamlary/mintsData/spectrumDiagrams/"+titleStr.replace(" ","")+".png")
         mO.closeDevice(deviceID)
     
     else:
