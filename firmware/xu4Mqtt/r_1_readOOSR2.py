6f0a3abee3728b7e2b007fd553eb85040545e17d
lakihaomal <lhw150030@utdallas.edu>
2024-02-01 15:36:44 -0600

    Additional Functionality to get spectrum details

--- firmware/xu4Mqtt/r_1_readOOSR2.py
+++ firmware/xu4Mqtt/r_1_readOOSR2.py
@@ -66,7 +66,7 @@ if __name__ == "__main__":
 
         mO.getAllSpectrumDetails(device)
 
-        mO.obtainTestSpectrums(device,\
+        mO.obtainDarkSpectrums(device,\
                             integrationTimeMicroSec)
 
         # waveLengths  = mO.getSpectrumDetails(device)
