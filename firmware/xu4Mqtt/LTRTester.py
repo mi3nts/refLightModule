b842c12075604ff8ab8149d49894f5f2ccbcd859
lakihaomal <lhw150030@utdallas.edu>
2024-02-08 14:31:47 -0600

    LTR Tester added

--- firmware/xu4Mqtt/LTRTester.py
+++ firmware/xu4Mqtt/LTRTester.py
@@ -95,4 +95,5 @@ if __name__ == '__main__':
             # val = sensor.ALS()
             print("UVS: %d"  %val)
             time.sleep(0.5)
-            
+    except KeyboardInterrupt:
+        exit()            
