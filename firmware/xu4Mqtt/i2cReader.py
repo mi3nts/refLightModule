605267fa12c7435724d93c59103aa1cb6d92c993
lakihaomal <lhw150030@utdallas.edu>
2024-02-09 16:26:32 -0600

    i2c debugging

--- firmware/xu4Mqtt/i2cReader.py
+++ firmware/xu4Mqtt/i2cReader.py
@@ -49,9 +49,9 @@ bus       = smbus2.SMBus(busNumber)
 
 scd30     = SCD30(bus,debug)
 bme280    = BME280(bus,debug)
-# as7265x   = AS7265X(bus,debug)
-# ltr390    = LTR390(bus,debug)
-# guvas12sd = GUVAS12SD(bus,debug,busNumber)
+as7265x   = AS7265X(bus,debug)
+ltr390    = LTR390(bus,debug)
+guvas12sd = GUVAS12SD(bus,debug,busNumber)
 
 # pa101d  = PAI101D_(bus,debug)
 
@@ -64,8 +64,8 @@ if __name__ == "__main__":
     print()
     
     # I2C Devices 
-    # as7265xOnline      =  as7265x.initiate()
-    # as7265xReadTime    = time.time()
+    as7265xOnline      =  as7265x.initiate()
+    as7265xReadTime    = time.time()
 
     bme280Online       =  bme280.initiate(30)
     bme280ReadTime     = time.time()
@@ -73,31 +73,31 @@ if __name__ == "__main__":
     scd30Online        =  scd30.initiate(30)
     scd30ReadTime      = time.time()
 
-    # ltr390Online       =  ltr390.initiate()
-    # ltr390ReadTime     = time.time()
+    ltr390Online       =  ltr390.initiate()
+    ltr390ReadTime     = time.time()
 
-    # guvas12sdOnline    =  guvas12sd.initiate()
-    # guvas12sdReadTime  = time.time()
+    guvas12sdOnline    =  guvas12sd.initiate()
+    guvas12sdReadTime  = time.time()
 
     delta = 10
 
     while True:
         try:    
-            # if as7265xOnline and mSR.getDeltaTimeAM(as7265xReadTime,delta):
-            #     as7265xReadTime  = time.time()
-            #     as7265x.readMqtt();
+            if as7265xOnline and mSR.getDeltaTimeAM(as7265xReadTime,delta):
+                as7265xReadTime  = time.time()
+                as7265x.readMqtt();
             if bme280Online and mSR.getDeltaTimeAM(bme280ReadTime,delta):
                 bme280ReadTime  = time.time()                
                 bme280.readMqtt();
             if scd30Online and mSR.getDeltaTimeAM(scd30ReadTime,delta):
                 scd30.readMqtt();
                 scd30ReadTime  = time.time()
-            # if  ltr390Online and mSR.getDeltaTimeAM(ltr390ReadTime,delta):
-            #     ltr390.readMqtt();
-            #     ltr390ReadTime  = time.time()
-            # if guvas12sdOnline and mSR.getDeltaTimeAM(guvas12sdReadTime,delta):
-            #     scd30.readMqtt();
-            #     guvas12sdReadTime  = time.time()       
+            if  ltr390Online and mSR.getDeltaTimeAM(ltr390ReadTime,delta):
+                ltr390.readMqtt();
+                ltr390ReadTime  = time.time()
+            if guvas12sdOnline and mSR.getDeltaTimeAM(guvas12sdReadTime,delta):
+                scd30.readMqtt();
+                guvas12sdReadTime  = time.time()       
 
         except Exception as e:
             time.sleep(.5)
