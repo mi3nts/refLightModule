c776fc11fbee486bd0de68beb897266e77e23c76
lakihaomal <lhw150030@utdallas.edu>
2024-02-21 16:58:19 -0600

    debugging

--- firmware/xu4Mqtt/mintsXU4/mintsOptics.py
+++ firmware/xu4Mqtt/mintsXU4/mintsOptics.py
@@ -940,7 +940,7 @@ def max_count_collector(device,electricDarkCorrelationUsage,nonLinearityCorrecti
                    columns=['Integration Time', 'Maximum'])], sort=False)
         print(result_df)
     # Find the maximum value closest to 75% of max cap
-    closest_to_75_percent = result_df.iloc[(result_df['Maximum'] - 0.75 * max_cap).abs().argsort()[0]]
+    closest_to_75_percent = result_df.iloc[(result_df['Maximum'] - 0.75 * mD.max_cap).abs().argsort()[0]]
  
     return closest_to_75_percent['Integration Time']
  
