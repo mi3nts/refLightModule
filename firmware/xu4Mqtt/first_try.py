d0d00e4f2f672b7630e1ee8a16d52a7005e81cad
lakihaomal <lhw150030@utdallas.edu>
2024-03-14 13:23:28 -0500

    first try edits

--- firmware/xu4Mqtt/first_try.py
+++ firmware/xu4Mqtt/first_try.py
@@ -75,8 +75,8 @@ totalWaitingTime       = 10
 #  At this point this is hardcoded 
 darkSpectrumFilePre         = "/home/teamlary/gitHubRepos/refLightModule/firmware/xu4Mqtt/darkSpectrums/Dark_Spectra_for_SN:SR200544-_EDCU:False-_NLCU:False-_IT:"
 
-
-darkSpectrumFilePost        = "-_StA:5-_BCW:5-_DT:2024-03-14_16:23:01_563827+00:00.pkl"
+# Dark_Spectra_for_SN:SR200544-_EDCU:False-_NLCU:False-_IT:2_5_s-_StA:5-_BCW:5-_DT:2024-03-14_18:19:13_385533+00:00.pkl
+darkSpectrumFilePost        = "-_StA:5-_BCW:5-_DT:2024-03-14_18:19:13_385533+00:00.pkl"
 
 
 
