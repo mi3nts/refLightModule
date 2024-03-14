adf730ffd64ed5992be4edc79a3f599f340454a0
lakihaomal <lhw150030@utdallas.edu>
2024-03-14 12:22:59 -0500

    first try edits

--- firmware/xu4Mqtt/first_try.py
+++ firmware/xu4Mqtt/first_try.py
@@ -76,7 +76,7 @@ totalWaitingTime       = 10
 darkSpectrumFilePre         = "darkSpectrums/Dark_Spectra_for_SN:SR200544-_EDCU:False-_NLCU:False-_IT:"
 
 
-darkSpectrumFilePost        = "_s-_StA:5-_BCW:5-_DT:2024-02-11_01:44:13_906446+00:00.pkl"
+darkSpectrumFilePost        = "-_StA:5-_BCW:5-_DT:2024-02-11_01:44:13_906446+00:00.pkl"
 
 
 
